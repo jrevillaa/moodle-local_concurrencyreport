70552fccd51dc4e5d85851c7d5173601c6d79cde
Jair Edson Revilla Arroyo <jrevilla@MacBook-Pro-de-Jair.local>
2026-03-23 11:06:02 -0500

    Prepare Moodle 4.1 release line

--- version.php
+++ version.php
@@ -17,8 +17,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_concurrencyreport';
-$plugin->version = 2026032310;
-$plugin->requires = 2025100600;
-$plugin->supported = array(501, 501);
-$plugin->release = '5.1.0';
+$plugin->version = 2026032304;
+$plugin->requires = 2022112800;
+$plugin->supported = array(401, 401);
+$plugin->release = '4.1.0';
 $plugin->maturity = MATURITY_STABLE;
